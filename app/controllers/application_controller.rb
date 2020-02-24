1e08ba9e4986caf90ebe270167271f5bd845b278
Dave Winniford <dwinniford@gmail.com>
2020-02-24 17:07:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,28 @@
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
-    set :views, 'app/views'
+    set :views,  'app/views' #something wrong here?? tried this from another lab Proc.new { File.join(root, "../views/") }
   end
+  
+  get '/recipes' do 
+    @recipes = Recipe.all 
+    erb :index 
+  end 
+  
+   get '/recipes/new' do 
+    erb :new
+  end 
+  
+  get '/recipes/:id' do 
+    @recipe = Recipe.find(params[:id])
+    erb :show 
+  end 
 
-  # code actions here!
+ 
+  
+  post '/recipes' do 
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect '/recipes/#{@recipe.id}'
+  end 
 
 end
